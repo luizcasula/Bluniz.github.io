d798327cefa0ad03594a37cd1142fa82416a811d
Lucas Rosa <55213609+Bluniz@users.noreply.github.com>
2020-05-16 00:29:24 -0300

    versão final

--- js/main.js
+++ js/main.js
@@ -2,7 +2,21 @@
 const titulo = document.querySelector('header div h1');
 const  subtitle = document.getElementById('sub');
 const  frase= document.getElementById('frase');
+const  sobreCorpo = document.getElementById('stats');
+const btn = document.getElementById('btn');
+const f1 = document.getElementById('f1');
+const f2 = document.getElementById('f2');
+const f3 = document.getElementById('f3');
 
+var i = 0;
+
+
+//teste
+console.log(btn);
+console.log(sobreCorpo);
+//
+
+btn.onclick = typeOpacity;
 
 function typeWriter(elemento){
   const arrayTxt = elemento.innerHTML.split("");
@@ -10,6 +24,21 @@ function typeWriter(elemento){
   arrayTxt.forEach((letra, i) =>{
     setTimeout(() =>{elemento.innerHTML += letra;}, 200*i)
   });
+}
+
+function typeOpacity(){
+  if(i === 0){
+  sobreCorpo.style.opacity = '1';
+  btn.innerHTML = 'Esconder';
+  i = 1;
+}  else{
+  sobreCorpo.style.opacity = '0';
+  btn.innerHTML = 'Clique aqui';
+  i = 0;
+
+}
+
+  
 }
 
 
